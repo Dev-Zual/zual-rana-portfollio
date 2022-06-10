fc7b9fe364fa39fbd5c024b596c0b042f0481ee3
Dev-Zual <freelancerzual@gmail.com>
2022-06-10 17:36:35 +0600

    created footer  section

--- src/pages/Home/ChoseMe.js
+++ src/pages/Home/ChoseMe.js
@@ -3,11 +3,14 @@ import ChoseCard from '../components/ChoseCard';
 
 const ChoseMe = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-      <ChoseCard />
-      <ChoseCard />
-      <ChoseCard />
-      <ChoseCard />
+    <div className="w-11/12 mx-auto mt-16">
+      <h1 className="text-7xl font-bold mb-16">WHY CHOOSE ME</h1>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-10">
+        <ChoseCard />
+        <ChoseCard />
+        <ChoseCard />
+        <ChoseCard />
+      </div>
     </div>
   );
 };
